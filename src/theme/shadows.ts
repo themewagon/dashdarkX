519f451368f34aeff11a84a5018c14bbd40e3c69
Riazul Islam <riazul5207@gmail.com>
2024-04-09 11:55:51 +0600

    theme shadows updated

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -1,3 +1,14 @@
-export default [
-    '0px 5px 14px 0px #0000000D',
+declare module '@mui/material/styles' {
+    interface Theme {
+        customShadows: string[]
+    }
+    interface ThemeOptions {
+        customShadows?: string[]
+    }
+}
+
+const customShadows = [
+    '0px 5px 14px 0px #0000000D'
 ];
+
+export default customShadows;
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,11 +2,13 @@ import { createTheme } from '@mui/material/styles';
 import palette from './palette';
 import typography from './typography';
 import Button from './components/Button';
+import customShadows from './shadows';
 
 export const theme = createTheme({
     typography,
     palette,
     components: {
         MuiButton: Button
-    }
+    },
+    customShadows
 });
