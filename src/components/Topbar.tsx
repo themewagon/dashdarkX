d85fcae57505300bbca325a6fe7c4d12cbae7065
Riazul Islam <riazul5207@gmail.com>
2024-04-10 18:47:24 +0600

    topbar component created

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Topbar = () => {
+    return (
+        <Box>
+            <Typography variant="subtitle1">Topbar</Typography>
+        </Box>
+    );
+}
+
+export default Topbar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Box, useTheme } from "@mui/material";
 import Sidebar from "components/Sidebar";
+import Topbar from "components/Topbar";
 import { ReactNode } from "react";
 
 interface LayoutProps {
@@ -10,9 +11,10 @@ const MainLayout = ({children}: LayoutProps) => {
     const theme = useTheme();
 
     return (
-        <Box sx={{margin: '0 auto', padding: '30px 20px', display: 'flex', width: '100%', maxWidth: '1920px', height: 'auto', minHeight: '100vh', backgroundColor: theme.palette.action.hover}}>
+        <Box sx={{margin: '0 auto', padding: '30px 20px', display: 'flex', gap: '20px', width: '100%', maxWidth: '1920px', height: 'auto', minHeight: '100vh', backgroundColor: theme.palette.action.hover}}>
             <Sidebar/>
             <Box sx={{width: 'calc(100%-265px)'}}>
+                <Topbar/>
                 {children}
             </Box>
         </Box>
