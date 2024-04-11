c61fded6308b7b8c9ce52000fbe5a4a0eb9132a5
Riazul Islam <riazul5207@gmail.com>
2024-04-11 16:19:47 +0600

    scrollbar ui updated

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,11 +11,13 @@ const MainLayout = ({children}: LayoutProps) => {
     const theme = useTheme();
 
     return (
-        <Box sx={{margin: '0 auto', padding: '30px 20px', display: 'flex', gap: '20px', width: '100%', maxWidth: '1920px', height: 'auto', minHeight: '100vh', backgroundColor: theme.palette.action.hover}}>
-            <Sidebar/>
-            <Box sx={{width: '100%'}}>
-                <Topbar/>
-                {children}
+        <Box sx={{width: '100%', height: 'auto', backgroundColor: theme.palette.action.hover}}>
+            <Box sx={{margin: '0 auto', padding: '30px 20px', display: 'flex', gap: '20px', width: '100%', maxWidth: '1920px', height: 'auto', minHeight: '100vh'}}>
+                <Sidebar/>
+                <Box sx={{width: '100%'}}>
+                    <Topbar/>
+                    {children}
+                </Box>
             </Box>
         </Box>
     );
