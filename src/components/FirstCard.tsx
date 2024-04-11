4061186cc646dbeb75fb713d951a9bbfa8b05b92
Riazul Islam <riazul5207@gmail.com>
2024-04-11 16:08:09 +0600

    first card section updated

--- src/components/FirstCard.tsx
+++ src/components/FirstCard.tsx
@@ -1,21 +1,28 @@
 import { Box, useTheme, Typography } from "@mui/material";
 import { Icon } from '@iconify/react';
 
-const FirstCard = () => {
+interface firstCardProps {
+    title: string,
+    value: string,
+    rate: string,
+    icon: string
+}
+
+const FirstCard = ({title, value, rate, icon}: firstCardProps) => {
     const theme = useTheme();
 
     return (
         <Box sx={{position: 'relative', padding: '20px', height: '130px', width: '100%', boxShadow: theme.customShadows[0], backgroundColor: '#fff', borderRadius: '20px'}}>
-            <Typography variant="caption" sx={{color: theme.palette.action.disabled, textTransform: 'uppercase'}}>TODAY’S MONEY</Typography><br/>
-            <Typography variant="h6" sx={{marginTop: '5px', color: theme.palette.neutral.light}}>$53,897</Typography>
+            <Typography variant="caption" sx={{color: theme.palette.action.disabled, textTransform: 'uppercase'}}>{title}</Typography><br/>
+            <Typography variant="h6" sx={{marginTop: '5px', color: theme.palette.neutral.light}}>{value}</Typography>
         
             <Box sx={{marginTop: '20px', display: 'flex', alignItems: 'center', gap: '6px'}}>
-                <Typography variant="subtitle2" sx={{color: theme.palette.success.light, fontWeight: 700}}>+3.48%</Typography>
+                <Typography variant="subtitle2" sx={{color: (rate[0] === '-') ? theme.palette.error.main : theme.palette.success.light , fontWeight: 700}}>{rate}</Typography>
                 <Typography variant="body2" sx={{color: theme.palette.action.disabled}}>Since last month</Typography>
             </Box>
 
             <Box sx={{position: 'absolute', top: '20px', right: '20px', height: '48px', width: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: theme.palette.secondary.main, borderRadius: '8px'}}>
-                <Icon icon={`ion:wallet`} style={{color: '#fff', fontSize: '1.3rem'}}/>
+                <Icon icon={`ion:${icon}`} style={{color: '#fff', fontSize: '1.4rem'}}/>
             </Box>
         </Box>
     );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,42 @@
 import { Box, Grid } from "@mui/material";
 import FirstCard from "components/FirstCard";
 
+const firstCardData = [
+    {
+        title: 'TODAY’S MONEY',
+        value: '$53,897',
+        rate: '+3.48%',
+        icon: 'wallet',
+    },
+    {
+        title: 'TODAY’S USERS',
+        value: '3,200',
+        rate: '+5.20%',
+        icon: 'globe-outline'
+    },
+    {
+        title: 'NEW CLIENTS',
+        value: '+2,503',
+        rate: '-2.82%',
+        icon: 'document-text'
+    },
+    {
+        title: 'TOTAL SALES',
+        value: '$173,000',
+        rate: '+8.12%',
+        icon: 'cart'
+    }
+];
+
 const Dashboard = () => {
     return (
         <Box sx={{width: '100%', height: '500px'}}>
             <Grid container spacing={2}>
-                <Grid item lg={3}>
-                    <FirstCard/>
-                </Grid>
-                <Grid item lg={3}>
-                    <FirstCard/>
-                </Grid>
-                <Grid item lg={3}>
-                    <FirstCard/>
-                </Grid>
-                <Grid item lg={3}>
-                    <FirstCard/>
-                </Grid>
+                {firstCardData.map((item) => {
+                    return <Grid item lg={3}>
+                        <FirstCard title={item.title} value={item.value} rate={item.rate} icon={item.icon}/>
+                    </Grid>
+                })}
             </Grid>
         </Box>
     );
