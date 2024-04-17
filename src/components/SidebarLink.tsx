4084da20433fdf3007dd1af2644c9f7d789ff830
Riazul Islam <riazul5207@gmail.com>
2024-04-17 13:32:08 +0600

    sidebar ui updated

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -1,4 +1,4 @@
-import { ListItemButton, useTheme, ListItemIcon, ListItemText } from "@mui/material";
+import { ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import { Icon } from '@iconify/react';
 
@@ -11,16 +11,16 @@ interface pathType {
 const SidebarLink = (props: pathType) => {
     const {name, path, icon} = props;
     const theme = useTheme();
-    // If (given path == current path) then the path is active, otherwise not
+    // check the path is active or not
     const isActivePath = useLocation().pathname.split('/')[1] === path;
 
     return (
         <Link to={`/${path}`} style={{textDecoration: 'none'}}>
             <ListItemButton sx={{boxShadow: `${isActivePath ? theme.customShadows[0] : null}`}}>
                 <ListItemIcon sx={{backgroundColor: `${isActivePath ? theme.palette.primary.main : null}`}}>
-                    <Icon icon={`ion:${icon}`} style={{color: `${isActivePath ? '#fff' : theme.palette.primary.main}`, fontSize: '1rem'}}/>
+                    <Icon icon={`ion:${icon}`} style={{color: `${isActivePath ? theme.palette.neutral.contrastText : theme.palette.primary.main}`, fontSize: '1rem'}}/>
                 </ListItemIcon>
-                <ListItemText>{name}</ListItemText>
+                <ListItemText sx={{color: `${isActivePath ? theme.palette.neutral.light : theme.palette.action.disabled}`}}>{name}</ListItemText>
             </ListItemButton>
         </Link>
     );
--- src/sections/Sidebar.tsx
+++ src/sections/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Hidden, Typography, useTheme, Button, Paper, ListItemButton } from "@mui/material";
+import { Box, Paper, Stack, Hidden, Typography, Button, ListItemButton, useTheme } from "@mui/material";
 import SidebarLink from "components/SidebarLink";
 import { Link } from "react-router-dom";
 
@@ -51,11 +51,11 @@ const Sidebar = () => {
     const theme = useTheme();
 
     return (
-        <Box sx={{height: 'calc(100vh - 60px)', overflow: 'hidden', borderRadius: '20px'}}>
-            <Box sx={{height: '100%', width: '100%', overflowY: 'scroll'}} component={Paper}>
+        <Box sx={{height: 'calc(100vh - 3.7rem)', overflow: 'hidden', borderRadius: '1.25rem'}}>
+            <Box component={Paper} sx={{height: '100%', width: '100%', overflowY: 'scroll'}}>
                 <Link to='/' style={{textDecoration: 'none'}}>
                     <ListItemButton sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img src={LogoImg} alt="logo" style={{marginRight: '8px', height: '24px', width: '24px'}}/>
+                        <img src={LogoImg} alt="logo" style={{marginRight: '0.5rem', height: '1.5rem', width: '1.5rem'}}/>
                         <Hidden mdDown>
                             <Typography variant="h5" color={theme.palette.neutral.main}>argon</Typography>
                         </Hidden>
@@ -66,28 +66,24 @@ const Sidebar = () => {
                     <img src={LineImg} alt="divider" style={{width: '100%', height: '1px'}}/>
                 </Box>
 
-                <Stack direction="column" sx={{marginTop: '22px'}}>
-                    {
-                        navLinks.map((item, index) => {
-                            return <SidebarLink key={index} path={item.path} name={item.name} icon={item.icon}/>
-                        })
-                    }
+                <Stack direction="column" sx={{marginTop: '1.375rem'}}>
+                    {navLinks.map((item, index) => {
+                        return <SidebarLink key={index} path={item.path} name={item.name} icon={item.icon}/>
+                    })}
                 </Stack>
 
-                <Stack direction="column" sx={{marginTop: '22px'}}>
+                <Stack direction="column" sx={{marginTop: '1.375rem'}}>
                     <Typography variant="caption" sx={{margin: '0 0 1rem 1rem', color: theme.palette.neutral.light, textTransform: 'uppercase'}}>Account Pages</Typography>
-                    {
-                        accountLinks.map((item, index) => {
-                            return <SidebarLink key={index} path={item.path} name={item.name} icon={item.icon}/>
-                        })
-                    }
+                    {accountLinks.map((item, index) => {
+                        return <SidebarLink key={index} path={item.path} name={item.name} icon={item.icon}/>
+                    })}
                 </Stack>
 
-                <Box sx={{marginTop: '3rem', paddingBottom: '20px'}}>
-                    <img src={docImg} alt="document" style={{height: '117px', width: '100%'}}/>
+                <Box sx={{marginTop: '3rem', paddingBottom: '1.25rem'}}>
+                    <img src={docImg} alt="document" style={{height: '7.313rem', width: '100%'}}/>
                     <Typography variant="body2" sx={{color: theme.palette.neutral.light, fontWeight: 700, textAlign: 'center'}}>Need Help?</Typography>
                     <Typography variant="subtitle1" sx={{color: theme.palette.action.active, fontSize: '0.75rem', textAlign: 'center'}}>Please check our docs</Typography>
-                    <Box sx={{marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px'}}>
+                    <Box sx={{marginTop: '1.25rem', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1.25rem'}}>
                         <Button color="primary" size="medium">Documentation</Button>
                         <Button color="secondary" size="medium">Upgrade to pro</Button>
                     </Box>
