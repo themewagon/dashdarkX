a27a46678bd7e05611f16c29dad60881b6b82c73
Riazul Islam <riazul5207@gmail.com>
2024-04-23 10:51:16 +0600

    drawer customization updated

--- src/theme/components/Drawer.tsx
+++ src/theme/components/Drawer.tsx
@@ -1,30 +1,28 @@
 import { Theme } from "@mui/material";
 import { Components } from "@mui/material/styles/components";
 
+const commonStyles = {
+    top: 30,
+    width: 265,
+    height: 'calc(100vh - 60px)',
+    boxSizing: 'border-box',
+    borderRight: 'none',
+    maxHeight: 910,
+}
+
 const Drawer: Components<Omit<Theme, 'components'>>['MuiDrawer'] = {
     styleOverrides: {
         root: {
             // desktop
             '& .css-rbkeor-MuiPaper-root-MuiDrawer-paper': {
                 position: 'sticky',
-                top: '30px',
-                width: '265px',
-                height: 'calc(100vh - 60px)',
-                boxSizing: 'border-box',
-                borderRight: 'none',
-                maxHeight: '910px',
+                ...commonStyles
             },
 
             // mobile
             '& .css-4kho5a-MuiPaper-root-MuiDrawer-paper': {
-                position: 'fixed',
-                top: '30px',
-                left: '30px',
-                width: '265px',
-                height: 'calc(100vh - 60px)',
-                boxSizing: 'border-box',
-                borderRight: 'none',
-                maxHeight: '910px',
+                position: 'fixed', left: 30,
+                ...commonStyles
             }
         }
     },
