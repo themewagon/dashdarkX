23191f7a5ab6f6a64794c8ae86bb0ecdd4ce6082
Riazul Islam <riazul5207@gmail.com>
2024-04-24 09:48:52 +0600

    data grid layout responsive done

--- src/sections/DashboardTables.tsx
+++ src/sections/DashboardTables.tsx
@@ -5,10 +5,10 @@ import SocialTrafficTable from "components/SocialTrafficTable";
 const DashboardTables = () => {
     return (
         <Grid container spacing={2}>
-            <Grid item lg={8}>
+            <Grid item xs={12} xl={8}>
                 <PageVisitsTable/>
             </Grid>
-            <Grid item lg={4}>
+            <Grid item xs={12} xl={4}>
                 <SocialTrafficTable/>
             </Grid>
         </Grid>
