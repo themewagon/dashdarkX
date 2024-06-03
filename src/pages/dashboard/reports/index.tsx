de9aa911a965bbe47f0e2a69f82bb769038040e2
Riazul Islam <riazul5207@gmail.com>
2024-06-03 14:37:01 +0600

    folder structure updated

--- src/pages/dashboard/reports/index.tsx
+++ src/pages/dashboard/reports/index.tsx
@@ -1,10 +1,10 @@
 import Grid from '@mui/material/Grid';
-import TopCards from 'components/sections/top-cards';
-import WebsiteVisitors from 'components/sections/website-visitors';
-import RevenueByCustomer from 'components/sections/revenue-by-customer';
-import Products from 'components/sections/products';
-import CompletedTask from 'components/sections/completed-task';
-import OrdersStatus from 'components/sections/orders-status';
+import TopCards from 'components/sections/dashboard/reports/top-cards';
+import WebsiteVisitors from 'components/sections/dashboard/reports/website-visitors';
+import RevenueByCustomer from 'components/sections/dashboard/reports/revenue-by-customer';
+import Products from 'components/sections/dashboard/reports/products';
+import CompletedTask from 'components/sections/dashboard/reports/completed-task';
+import OrdersStatus from 'components/sections/dashboard/reports/orders-status';
 
 const Reports = () => {
   return (
