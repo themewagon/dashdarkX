b888804ee67ef890ae1a82222d34a80a1d88378e
Riazul Islam <riazul5207@gmail.com>
2024-06-09 16:37:34 +0600

    footer section completed

--- src/layouts/main-layout/Footer.tsx
+++ src/layouts/main-layout/Footer.tsx
@@ -1,10 +1,20 @@
 import Typography from '@mui/material/Typography';
-// import Link from '@mui/material/Link';
+import Link from '@mui/material/Link';
 
 const Footer = () => {
   return (
-    <Typography mt={1} px={1} color="text.secondary" variant="body2" align="right" letterSpacing={0.5}>
-      Footer text
+    <Typography
+      mt={1}
+      px={1}
+      color="text.secondary"
+      variant="body2"
+      align="right"
+      letterSpacing={0.5}
+    >
+      Made with ❤️ by{' '}
+      <Link href="https://themewagon.com/" target="_blank" rel="noreferrer">
+        {'ThemeWagon'}
+      </Link>
     </Typography>
   );
 };
