75e7d3ba67ba3208f78d3d48549965e68549db2b
Riazul Islam <riazul5207@gmail.com>
2024-06-11 14:53:57 +0600

    Updates

--- assets/App-DK_fCuRS.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-BHT3Q13r.js";const e=()=>t.jsx(s,{});export{e as default};
---
+++ assets/App-jttnfvgd.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-C8aS7bsq.js";const e=()=>t.jsx(s,{});export{e as default};
